727f9e33e97f011b98f3843d5af06e3d3e2ae99e
ConstantMotion1 <constan.fernando@hotmail.com>
2022-10-19 21:23:34 -0400

    created sql schema and seed files

---
+++ db/seeds.sql
@@ -0,0 +1,42 @@
+INSERT INTO department (name)
+VALUES  ("Marketing"),
+        ("Finance"),
+        ("Human Resources"),
+        ("IT"),
+        ("Sales"),
+        ("Production");
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Lead Marketing Analyst", 80000, 1),
+        ("Junior Marketing Analyst", 45000, 1),
+        ("Lead Accountant", 100000, 2),
+        ("Junior Accountant", 60000, 2),
+        ("HR", 60000, 3),
+        ("Lead IT Analyst", 120000, 4),
+        ("Junior IT Analyst", 90000, 4),
+        ("Lead Sales Analyst", 130000, 5),
+        ("Junior Sales Analyst", 90000, 5),
+        ("Production Supervisor", 60000, 6),
+        ("Production Worker", 40000, 6);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Lukas", "Wolf", 1, NULL),
+        ("Amaris", "Norman", 2, 1),
+        ("Frida", "Long", 2, 1),
+        ("Jose", "Diaz", 3, NULL),
+        ("Jaylon", "Best", 4, 4),
+        ("Aiden", "McClain", 4, 4),
+        ("Molly", "Deleon", 5, NULL),
+        ("Elena", "Shelton", 6, NULL),
+        ("Autumn", "Dillon", 7, 8),
+        ("Alessandro", "Holt", 7, 8),
+        ("Elisa", "Patrick", 8, NULL),
+        ("Jaelyn", "Terrell", 9, 11),
+        ("Kai", "Neal", 9, 11),
+        ("Yurem", "Sullivan", 10, NULL),
+        ("Mallory", "Key", 11, 14),
+        ("Ace", "Garza", 11, 14),
+        ("Thomas", "Skinner", 11, 14);
+        
+
+
